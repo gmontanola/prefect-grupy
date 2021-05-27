8d128d44a92f491d36ed5fca6fbab56c3c328c81
Gabriel Montañola <gabriel@montanola.com>
2021-05-27 05:43:19 -0300

    :pencil2: typo on shell task

--- exemplos/conceitos/biblioteca_tarefas.py
+++ exemplos/conceitos/biblioteca_tarefas.py
@@ -2,7 +2,7 @@ from prefect import Flow
 from prefect.tasks.shell import ShellTask
 
 pwd_task = ShellTask(name="Onde estou?", command="pwd", stream_output=True)
-whoami_task = ShellTask(name="Quem sou eu?", command="pwd", stream_output=True)
+whoami_task = ShellTask(name="Quem sou eu?", command="whoami", stream_output=True)
 
 with Flow("biblioteca_tarefas") as flow:
     pwd = pwd_task()
